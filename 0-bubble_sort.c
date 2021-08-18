fea24495a4432b4139e2fdd065ce3753eca5c15e
Adam Taylor <tayloradam1999@gmail.com>
2021-08-18 02:31:23 +0000

    Pushing file for task 0 onto taylor branch with comments

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,23 +9,29 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t tmp;
-	int is_swapped;
+	size_t tmp; /* store values */
+	int is_swapped; /* check if swap has been made */
 
+	/* loop through array */
 	for (size_t i = 0; i < size; i++)
 	{
+		/* re-initalize to 0 after every loop */
 		is_swapped = 0;
 		for (size_t j = 0; j < size - 1 - i; j++)
 		{
+			/* compare adjacent elements */
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+				/* set is_swapped to 1 to indicate */
+				/* that we made a swap */
 				is_swapped = 1;
 			}
 			print_array(array, size);
 		}
+		/* if no swaps, exit */
 		if (!is_swapped)
 			break;
 	}
