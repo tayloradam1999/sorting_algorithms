9ddde118ce7ca68b428903ec8363da20091aec28
Rachel Lewis <RLewis11769@gmail.com>
2021-08-18 01:38:51 +0000

    Add edge cases and print statement to 2 - think it's printing one too many times

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * selection_sort - selection sort
+ * selection_sort - selection sort to find smallest element each loop
  * @array: array to sort
  * @size: size of array
  */
@@ -11,7 +11,12 @@ void selection_sort(int *array, size_t size)
 	int min_index, temp;
 	size_t curr, next;
 
-	if (array && size > 1)
+	if (size <= 1)
+		return;
+	if (size == 2 && array[0] < array[1])
+		return;
+
+	if (array)
 	{
 		for (curr = 0; curr < size - 1; curr++)
 		{
@@ -27,6 +32,8 @@ void selection_sort(int *array, size_t size)
 			temp = array[curr];
 			array[curr] = array[min_index];
 			array[min_index] = temp;
+
+			print_array(array, size);
 		}
 	}
 }
